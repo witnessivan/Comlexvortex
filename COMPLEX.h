2e1b95ad28dea804d8b254a1d156d124719d4d40
Witnessvan <32366597+witnessivan@users.noreply.github.com>
2018-03-04 21:10:17 +0300

    Create COMPLEX.h

---
+++ COMPLEX.h
@@ -0,0 +1,20 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+#include<iostream>
+#include <cmath>
+#include <string.h>
+using namespace std;
+typedef struct vortex * complex;
+typedef struct vortex {
+	double real;
+	double image;
+
+}vortex;
+
+void print_interface(char select);
+complex print_Complex(complex z);
+vortex EnterComplex(complex x, complex y);
+vortex Sum(complex x, complex y);
+vortex Substract(complex x, complex y);
+double multiply(complex x, complex y);
+#endif
